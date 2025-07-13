9d239e3756c47850c1de73160b4fc9a140eda62d
Sitnic Vladimir <asleepcarpenterdev@gmail.com>
2025-07-14 01:40:54 +0300

    So ass ah code but works hooray

---
+++ netlify/functions/fetch-dom.ts
@@ -0,0 +1,25 @@
+export const handler = async (event) => {
+  const url = event.queryStringParameters.url;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`An error occured ${response.status}`);
+
+    const html = await response.text();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: html,
+      }),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "FUCK U!",
+      }),
+    };
+  }
+};
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 const insertUrlForm = document.getElementById("url-input-form");
-const rmPlayersForm = document.getElementById("rm-players-form");
+const table = document.getElementById("table");
+const shuffleButton = document.getElementById("shuffle");
+
+interface TableData {
+  [key: number]: string;
+}
+
+let tableData = new Map<number, string>();
 
 insertUrlForm!.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -8,21 +15,91 @@ insertUrlForm!.addEventListener("submit", async (e) => {
   ) as HTMLInputElement;
   const url = inputElement.value;
 
-  // try {
-  //   const response = await fetch(url);
-  //   if (!response.ok) throw new Error(`An error occured ${response.status}`);
+  const htmlObj = await fetch("/.netlify/functions/fetch-dom?url=" + url).then(
+    (response) => response.json()
+  );
 
-  //   const html = await response.text();
-  //   const parcer = new DOMParser();
-  //   const doc = parcer.parseFromString(html, "text/html");
+  const parcer = new DOMParser();
+  const doc = parcer.parseFromString(htmlObj.message, "text/html");
 
-  //   const table = doc.querySelector("table");
-  //   console.log(table);
-  // } catch (err) {
-  //   console.error(err);
-  // }
+  const tableHtmlElement = doc.querySelector("table");
+  tableData = extractTableData(tableHtmlElement) as Map<number, string>;
+  renderTable(tableData);
 });
 
-rmPlayersForm!.addEventListener("submit", (e) => {
-  e.preventDefault();
+table!.addEventListener("click", (e) => {
+  const el = e.target as HTMLElement;
+  const rank = el.closest("tr")?.dataset.rank;
+  if (rank) {
+    tableData.delete(+rank);
+    renderTable(tableData);
+  }
 });
+
+shuffleButton!.addEventListener("click", (e) => {
+  renderTable(tableData, "shuffle");
+});
+
+function extractTableData(table: HTMLTableElement | null) {
+  if (table === null) return {};
+
+  const data = new Map<number, string>();
+
+  const rows = Array.from(table.rows);
+
+  for (const row of rows) {
+    const cells = Array.from(row.cells);
+    if (
+      cells.length >= 2 &&
+      cells[0].tagName.toLowerCase() === "td" &&
+      cells[1].tagName.toLowerCase() === "td"
+    ) {
+      data.set(+cells[0].textContent!.trim(), cells[1].textContent!.trim());
+    }
+  }
+
+  return data;
+}
+
+function renderTable(
+  tableData: Map<number, string>,
+  type: "shuffle" | "normal" = "normal"
+) {
+  if (!tableData) return;
+
+  document.getElementById("shuffle")!.classList.remove("hidden");
+  document.querySelector(".table-container")!.classList.remove("hidden");
+
+  const dataArr = Array.from(tableData);
+  if (type === "shuffle") shuffle(dataArr);
+
+  table!.innerHTML = `
+      <tr>
+      <th>Rank</th>
+      <th>Name</th>
+      </tr>
+      ${dataArr
+        .map(
+          (val) =>
+            `<tr data-rank=${val[0]}><td>${val[0]}</td><td>${val[1]}</td></tr>`
+        )
+        .join("")}
+    `;
+}
+
+function shuffle(array: [number, string][]) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
